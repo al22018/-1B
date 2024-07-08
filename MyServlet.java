06f12dae38830c06248710888f226d95faac016b
al22018 <al22018@shibaura-it.ac.jp>
2024-07-08 17:35:40 +0900

    Update MyServlet.java

--- MyServlet.java
+++ MyServlet.java
@@ -11,11 +11,11 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/api/data")
 public class MyServlet extends HttpServlet {
@@ -59,8 +59,8 @@ public class MyServlet extends HttpServlet {
                 JoinRegister joinRegister = new JoinRegister();
                 joinRegister.sendData(Integer.parseInt((String) requestData.get("projectID")),(String) requestData.get("displayName"),2);
             } else if("dispose".equals(action)) {
-            	EventRegister eventRegister = new EventRegister();
-            	eventRegister.disposeData(requestData);
+                EventRegister eventRegister = new EventRegister();
+                eventRegister.disposeData(requestData);
             }
 
             String jsonResponseString = jsonResponse.toJSONString();
@@ -84,7 +84,7 @@ public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
         JSONArray jsonArray = new JSONArray();
-        
+
         try {
             String projectIDParam = request.getParameter("projectID");
 
@@ -127,3 +127,4 @@ public class MyServlet extends HttpServlet {
     }
 
 }
+
