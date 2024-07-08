5bcd383fc708c59327b105a68dea39437ad075e4
al22018 <al22018@shibaura-it.ac.jp>
2024-07-08 17:36:12 +0900

    Update JoinRegister.java

--- JoinRegister.java
+++ JoinRegister.java
@@ -12,7 +12,7 @@ public class JoinRegister {
 		    UserAndProjectInfo userproject = new UserAndProjectInfo;
 		    useproject.userID=userID;
 		    useproject.projectID=projectID;
-		    setUserAndProjectInfo()
+		    setUserAndProjectInfo();
 		}*/
 
 		for (JSONObject data : MyServlet.jsonDataList) {
@@ -31,7 +31,7 @@ public class JoinRegister {
 				boolean participantExists = false;
 				for (Object obj : participantsArray) {
 					JSONObject participant = (JSONObject) obj;
-					if ((Integer)participant.get("userID") == userID) {
+					if ((Integer) participant.get("userID") == userID) {
 						participantExists = true;
 						break;
 					}
@@ -39,14 +39,16 @@ public class JoinRegister {
 
 				// Add the participant if they do not already exist
 				if (!participantExists) {
-					JSONObject participant = new JSONObject();
-					participant.put("displayName", displayName);
-					participant.put("userID", userID);
-					participantsArray.add(participant);
+					ProjectInfo cons = new ProjectInfo();
+					ProjectInfo project = cons.getProjectInfo(projectID);
+					if ("Registration".equals(project.progressStatus)) {
+
+						JSONObject participant = new JSONObject();
+						participant.put("displayName", displayName);
+						participant.put("userID", userID);
+						participantsArray.add(participant);
+					}
 				}
-
-				break;
-
 			}
 		}
 	}
